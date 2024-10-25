c573d397c0bad6d81a9ddae7b1192d6999d00787
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-25 04:54:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,68 @@
+from __future__ import annotations
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None: # noqa
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(
+            self.x + other.x,
+            self.y + other.y
+        )
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(
+            self.x - other.x,
+            self.y - other.y
+        )
+
+    def __mul__(self, other: Vector | float) -> Vector | float:
+        if isinstance(other, Vector):
+            return (self.x * other.x) + (self.y * other.y)
+        return Vector(
+            self.x * other,
+            self.y * other
+        )
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
+
+    def get_length(self) -> float:
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def get_normalized(self) -> Vector:
+        return Vector(
+            self.x / self.get_length(),
+            self.y / self.get_length()
+        )
+
+    def angle_between(self, other: Vector) -> int:
+        cos_a = (self * other) / (self.get_length() * other.get_length())
+        return math.ceil(math.degrees(math.acos(cos_a)))
+
+    def get_angle(self) -> int:
+        angle = round(math.degrees(math.atan2(self.x, self.y)))
+        if angle < 0:
+            angle *= -1
+        return angle
+
+    def rotate(self, degrees: int) -> Vector:
+        radians = math.radians(degrees)
+        cos_angle = math.cos(radians)
+        sin_angle = math.sin(radians)
+
+        return Vector(
+            self.x * cos_angle - self.y * sin_angle,
+            self.x * sin_angle + self.y * cos_angle
+        )
